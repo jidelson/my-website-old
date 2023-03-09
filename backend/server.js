cd72b908bff4f1a9e15a8963b02880bf32cdf666
jidelson <joeidelson@gmail.com>
2023-03-09 17:26:47 -0600

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../client/public')));
+
+app.get('/api', (req, res) => {
+  res.json({
+      message: 'This is the api endpoint'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/public/index.html'));
+});
+
+
+app.listen(port, function(){
+  console.log(`server started on port ${port} `)
+})
