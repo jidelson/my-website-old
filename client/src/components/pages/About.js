23742a1785d7d4483648cc843ad9349d7e416cf4
jidelson <joeidelson@gmail.com>
2023-03-28 15:57:14 -0500

    rearranging about pg

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -11,33 +11,86 @@ function About() {
     return (
         <AboutContainer>
             <div className="container-fluid">
-                <div className="topRow row">
-                    <div className="col-lg-4 col-md-12">
-                        <div className="headshot-image py-4">
-                            <img src={headshot} alt="headshot" className="headshot img-fluid" />
-                        </div>
+                <div className="row">
+                    <div className="col-md-4 py-4">
+                        <img src={headshot} alt="headshot" className="headshot img-fluid" />
                     </div>
-                    <div className="col-lg-8 col-md-12 about-title">
-                        <h2 className="heyThere text-uppercase pt-5">
-                            Hey there!
-                        </h2>
-                        <div className="paragraph py-4">
-                            <p className="para">
-                                I'm Joe Idelson, a full stack web developer.   
-                            </p>
-                            <p className="para">
-                                I learned my development skills through Lambda School (now called Bloom Institute of Technology), a nine month intensive boot-camp style school, where I was 
-                                taught the most contemporary techniques and skills for web development. Lambda School constantly updates its curriculum based on what tools and techniques 
-                                are currently being used in the industry. While attending Lambda School I was also able to be a mentor to students in earlier units of the curriculum.    
-                            </p>
-                            <p className="para">
-                            Below you can see my badge of completion for the Lambda School curriculum and some of what it entailed.
-                            </p>
-                        </div>
+                    <div className="col-md-4" style={{paddingRight: "5%"}} >
+                        <h2 className="text-uppercase pt-5">Hey there!</h2>
+                        <p className="para pt-3">I'm Joe Idelson, a full stack web developer.</p>
+                        <p className="para">I was born in the beautiful city of San Francisco and grew up in the city of Walnut Creek, CA.</p>
+                    </div>
+                    <div className="col-md-4 py-4">
+                        <img src={bayB} alt="Bay Bridge & SF Skyline" style={{maxWidth: "100%"}} />
                     </div>
                     
                 </div>
             </div>
+
+
+                    
+
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className="col-md-4 py-4">
+                        <img src={wc} alt="walnutCreek" style={{maxWidth: "100%"}} />
+                    </div>
+                    <div className='col-md-4 py-4'>
+                        <p className="para text-center py-5">
+                            After graduating high school and attending a year at the University of Oregon I enrolled in a
+                            digital arts college called Expression College. 
+                        </p>
+                        <p className="para text-center py-1">
+                            It was there that I was introduced, taught, and learned many new technologies used for production.
+                            I gained the ability to learn new programs at a rapid rate which directly translated to my transition
+                            into web development.
+                        </p>
+                        <p className="para text-center py-1">
+                            After graduating Expression, I worked as a freelancer doing many different projects from graphic design
+                            to fully rendered 3D animations. 
+                        </p>
+                        <p className="para text-center py-1">
+                            During this time, on a daily basis I was using the Adobe Suite and primarily Autodesk Maya, along with
+                            a number of other programs. As time progressed, I noticed I was drawn more towards the skills in the 
+                            animation field that were more technical including rigging. 
+                        </p>
+                    </div>
+                    <div className="col-md-4 py-4">
+                        <img src={expression} alt="expression-img" className="expression img-fluid" />
+                    </div>
+                </div>
+            </div>
+
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-md-4 py-4'>
+                        <img src={lambda} alt="lambda-img" className="lambda img-fluid" />    
+                    </div>
+                    <div className='col-md-4 py-4'>
+                        <p className="para">
+                            I learned my development skills through Lambda School (now called Bloom Institute of Technology), a nine month intensive boot-camp style school, where I was 
+                            taught the most contemporary techniques and skills for web development. Lambda School constantly updates its curriculum based on what tools and techniques 
+                            are currently being used in the industry.     
+                        </p>
+                        <p className="para">
+                               The curriculum at Lambda included learning through lectures and assignments and then
+                               completing weekly sprints. There were also group projects where projects would be planned and completed. Students would also interact with stakeholders to
+                               gain more experience of a professional working environment. While attending Lambda School I was also able to be a mentor to students in earlier units of 
+                               the curriculum.
+                            </p>
+                        <p className="para">
+                            Below you can see my badge of completion for the Lambda School curriculum and some of what it entailed.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+           
+
+
+
 
             <div className="text-center">
                 <a className="btn button primary-button mr-4 text-uppercase" href="https://drive.google.com/file/d/1urVavN1ztUdGnO6UMR6UIm1xZEU3Yqxl/view?usp=sharing">
@@ -46,79 +99,28 @@ function About() {
             </div>
 
             <div className="container-fluid">
-                <div className="whereImFromRow row">
+                <div className="row">
                     <div className="col-lg-4 col-md-12">
-                        <div className="py-4">
-                            <img src={bayB} alt="bayBridge_SFskyline-img" className="bayB img-fluid" />
-                        </div>
-                    </div>
-
-                    <div className="col-lg-4 col-md-12 about-title">
-                        <div className="bornText paragraph py-4 w-75">
-                            <p className="para text-center py-5 mt-5 pl-4 ml-5">
-                                I was born in the beautiful city of San Francisco and grew up in the city of Walnut Creek, CA.   
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-12">
-                        <div className="py-4">
-                            <img src={wc} alt="walnutCreek" className="wc img-fluid" />
-                        </div>
+                        
+                            
                     </div>
                 </div>
             </div>
 
-            <div className="container-fluid">
-                <div className="expression-container row">
-                    <div className="expressionRow col-lg-4 col-md-12">
-                        <div className="expression-image py-4">
-                            <img src={expression} alt="expression-img" className="expression img-fluid" />
-                        </div>
-                    </div>
-
-                    <div className="col-lg-8 col-md-12 about-title">
-                        <div className="expressionText paragraph py-4">
-                            <p className="para text-center py-5">
-                                After graduating high school and attending a year at the University of Oregon I enrolled in a
-                                digital arts college called Expression College. 
-                            </p>
-                            <p className="para text-center py-1">
-                                It was there that I was introduced, taught, and learned many new technologies used for production.
-                                I gained the ability to learn new programs at a rapid rate which directly translated to my transition
-                                into web development.
-                            </p>
-                            <p className="para text-center py-1">
-                                After graduating Expression, I worked as a freelancer doing many different projects from graphic design
-                                to fully rendered 3D animations. 
-                            </p>
-                            <p className="para text-center py-1">
-                                During this time, on a daily basis I was using the Adobe Suite and primarily Autodesk Maya, along with
-                                a number of other programs. As time progressed, I noticed I was drawn more towards the skills in the 
-                                animation field that were more technical including rigging. 
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+         
   
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-lg-8 col-md-12 about-title">
-                        <div className="lambdaText paragraph py-4 w-75">
-                            <p className="para text-center py-5">
-                               I then enrolled in Lambda School to learn full stack web development. The curriculum at Lambda included learning through lectures and assignments and then
-                               completing weekly sprints. There were also group projects where projects would be planned and completed. Students would also interact with stakeholders to
-                               gain more experience of a professional working environment. 
-                            </p>
+                    <div className="col-lg-8 col-md-12">
+                        <div className="lambdaText py-4 w-75">
+                           
                         </div>
 
                        
                     </div>
 
                     <div className="lambda-image col-lg-4 col-md-12">
-                        <div className="py-4">
-                            <img src={lambda} alt="lambda-img" className="lambda img-fluid" />
-                        </div>
+                        
                     </div>
                 </div>
             </div>
@@ -153,7 +155,7 @@ export default About;
 const AboutContainer = styled.div`
 
 .headshot{
-    width: 80%;
+    width: 60%;
 
 }
 
