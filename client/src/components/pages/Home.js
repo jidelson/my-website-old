882784d34f0cd14d032dd30585e916cf8f7c379c
jidelson <joeidelson@gmail.com>
2023-04-13 16:35:36 -0500

    added angular

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -22,6 +22,7 @@ import linux from '../../images/linux.png';
 import mysql from '../../images/mysql.png';
 import github from '../../images/github.png';
 import spring from '../../images/spring.png';
+import angular from '../../images/angular.png';
 
 import vbb from '../../images/vbb.png';
 import cookbook from '../../images/cookbook.jpg';
@@ -278,6 +279,16 @@ function Home() {
                                 </div>
                                 <div className="card-body text-center">
                                     <h4 className="card-title font-roboto">Spring</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <div className="skills">
+                                <div className="skills-img text-center-py-4">
+                                    <img src={angular} alt="Angular" className="rounded mx-auto d-block img-fluid" />
+                                </div>
+                                <div className="card-body text-center">
+                                    <h4 className="card-title font-roboto">Angular</h4>
                                 </div>
                             </div>
                         </div>
