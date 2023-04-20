bdfe71d8a9c55b57d0db469ed12dc2e669429bfb
jidelson <joeidelson@gmail.com>
2023-04-19 20:31:31 -0500

    switched start script & procfile to server.js, moved index.js to root

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './client/src/App';
+import reportWebVitals from './client/src/reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
