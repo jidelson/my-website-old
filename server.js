a1cbaa28f9722d761a9de58aef9d379fabd7a012
jidelson <joeidelson@gmail.com>
2023-04-20 14:28:04 -0500

    fixed path in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(express.static(path.join(__dirname, './client/public')));
 
 app.get('/api', (req, res) => {
   res.json({
@@ -12,7 +12,7 @@ app.get('/api', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, './client/public/index.html'));
 });
 
 
